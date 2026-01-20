4598fc3dd4050d136694ed92d95c6b98e8b88b8e
Ashwin Muthukumar <ashwinmuthukumar2001@gmail.com>
2026-01-20 21:26:38 +0530

    singly linkedlist operations

--- Linked List/SinglyLinkedList.py
+++ Linked List/SinglyLinkedList.py
@@ -29,9 +29,41 @@ class LinkedList:
             current = current.next
         print("None")
 
+    #insert node at beginning
+    def insertAtBegining(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    #insert node at specific position(0 index based)
+    def insertAtIndex(self,data,index):
+        new_node = Node(data)
+        if index == 0:
+            self.insertAtBegining(data)
+            return
+        
+        current = self.head
+        for i in range(index-1):
+            if current:
+                current = current.next
+            else:
+                print('index exceeds length of the list')
+                return
+
+        new_node.next = current.next
+        current.next = new_node
+        
+        
+ 
 
 L = LinkedList()
 L.append(10)
 L.append(20)
 L.append(30)
+L.insertAtBegining(5)
+L.insertAtIndex(15,2)
+L.insertAtIndex(40,5)
+
+L.insertAtIndex(100,10)
+
 L.display()
